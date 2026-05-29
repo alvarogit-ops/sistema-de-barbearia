28751713b97ff2885abd3df068ffdb96a265ca8e
Álvaro Yuri <alvaro.yuri@outlook.com>
2026-05-29 09:38:19 -0300

    Deixando campos passíveis de serem vazios para evitar erro de migrações

--- barber_grid/models.py
+++ barber_grid/models.py
@@ -4,17 +4,17 @@ from django.db import models
 class Servico(models.Model):
     """Tipos de serviço que a barbearia oferece, tipo cabelo, barba, sobrancelha"""
     nome = models.CharField(max_length=100)
-    preco = models.DecimalField(max_digits=5, decimal_places=2)
+    preco = models.DecimalField(max_digits=5, null=True, decimal_places=2)
 
     def __str__(self):
         return self.nome
 
 class Agendamento(models.Model):
     """Um agendamento para barbearia marcado pelo cliente"""
-    cliente_nome = models.CharField(max_length=100)
-    servico = models.ForeignKey(Servico, on_delete=models.CASCADE)
+    cliente_nome = models.CharField(max_length=100, null=True, blank=True)
+    servico = models.ForeignKey(Servico, on_delete=models.PROTECT, null=True, blank=True)
 
-    data_agendada = models.DateTimeField()
+    data_agendada = models.DateTimeField(null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
