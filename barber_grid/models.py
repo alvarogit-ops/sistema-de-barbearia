df2f16ea5c40f39a09763b9601cc599ebfe5fcb4
Álvaro Yuri <alvaro.yuri@outlook.com>
2026-05-29 09:30:06 -0300

    Salvamento de estado com erro de validação no campo de data

--- barber_grid/models.py
+++ barber_grid/models.py
@@ -1,14 +1,26 @@
 from django.db import models
 
+
+class Servico(models.Model):
+    """Tipos de serviço que a barbearia oferece, tipo cabelo, barba, sobrancelha"""
+    nome = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.nome
+
 class Agendamento(models.Model):
-    """Um agendamento para barbearia"""
-    servico = models.CharField(max_length=100)
+    """Um agendamento para barbearia marcado pelo cliente"""
+    cliente_nome = models.CharField(max_length=100)
+    servico = models.ForeignKey(Servico, on_delete=models.CASCADE)
+
+    data_agendada = models.DateTimeField()
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        """Devolve uma representação em string do modelo"""
-        return self.servico  
-    
+        return f"{self.cliente_nome} - {self.servico.nome}"
 
-class Entry(models.Model):
-    """Algo específico aprendido sobre um assunto."""
+       
+                                
+                                
+                                
