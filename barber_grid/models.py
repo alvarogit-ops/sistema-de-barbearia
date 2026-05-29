ca2312abde61c93ac61f4e216cffd9d651923b78
Álvaro Yuri <alvaro.yuri@outlook.com>
2026-05-29 11:24:03 -0300

    fix: corrige models.py com blank=True que estava sem null=True e outros erros de sintaxe e identação na f-string

--- barber_grid/models.py
+++ barber_grid/models.py
@@ -12,13 +12,14 @@ class Servico(models.Model):
 class Agendamento(models.Model):
     """Um agendamento para barbearia marcado pelo cliente"""
     cliente_nome = models.CharField(max_length=100, null=True, blank=True)
-    servico = models.ForeignKey(Servico, on_delete=models.PROTECT, null=True, blank=True)
+    servico = models.ForeignKey(Servico, on_delete=models.CASCADE, null=True, blank=True)
 
     data_agendada = models.DateTimeField(null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.cliente_nome} - {self.servico.nome}"
+        nome_servico = self.servico.nome if self.servico else "Sem serviço" #tudo na mesma linha
+        return f"{self.cliente_nome or 'Anônimo - {nome_servico}'}"
 
        
                                 
