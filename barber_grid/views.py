9b5e082f29ab4ce89ed2cb2ff997293e31c10726
Álvaro Yuri <alvaro.yuri@outlook.com>
2026-06-04 13:34:37 -0300

    Importa models nas views e renderiza o contexto, criando um agendamento com objetos da variável data agendada

--- barber_grid/views.py
+++ barber_grid/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Agendamento
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'barber_grid/index.html')
+
+
+def agendamento(request):
+
+    agendamento = Agendamento.objects.order_by('data_agendada')
+
+    context = {'agendamentos': agendamento}
+    return render(request, 'barber_grid/agendamento.html', context)
