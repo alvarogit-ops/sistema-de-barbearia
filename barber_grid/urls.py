37bd5fe919db90087281d0b9b0eabe2edb7bc789
Álvaro Yuri <alvaro.yuri@outlook.com>
2026-07-01 20:33:18 -0300

    adiciona rota para cadastro

--- barber_grid/urls.py
+++ barber_grid/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('agendamento', views.agendamento, name="agendamento"),
-    path('agendamento<cliente_id>/', views.agendamento, name='agendamento') #como vou adicionar o cliente_id aqui? #vou precisar de uma view nova para isso? 
+    path('agendamento<cliente_id>/', views.agendamento, name='agendamento'),
+    path('cadastro/', views.cadastro, name='cadastro'),
 ]
+
