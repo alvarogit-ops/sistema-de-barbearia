e9f24b43484d5edf100189e09e385bf1c3fedd63
Álvaro Yuri <alvaro.yuri@outlook.com>
2026-07-06 22:47:34 -0300

    Melhoria do primeiro código com laços for

--- study/ex001.py
+++ study/ex001.py
@@ -1,6 +1,12 @@
-cliente = str(input("Digite o nome do cliente: "))
-
 lista = []
-lista.append(cliente)
-print(f"Cliente cadastrado com sucesso: {cliente}")
+cont = 0
+n = int(input("Quantos clientes quer cadastrar? "))
 
+for l in range(n):
+    cont = cont + 1
+    cliente = str(input(f"Digite o nome do {cont}o cliente: "))
+    print(f"Cliente cadastrado com sucesso: {cliente}")
+
+    lista.append(cliente)
+for cliente in lista:
+    print(f"Clientes cadastrados: {cliente}")
