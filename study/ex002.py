970925b63678a9358ad5909acb83d93b69464e1e
Álvaro Yuri <alvaro.yuri@outlook.com>
2026-07-08 22:15:08 -0300

    ex002 parcialmente resolvido

---
+++ study/ex002.py
@@ -0,0 +1,28 @@
+# Cadastrar barbeiros:
+
+from datetime import date
+
+ano_atual = date.today().year
+
+n = int(input("Quantos barbeiros tem na barbearia? "))
+
+lista_principal = []
+for cont in range(1, n + 1):
+    barbeiro = str(input(f"Digite o nome do {cont} barbeiro: "))
+    ano_nasc = int(input("Qual ano de nascimento dele? "))
+
+    idade = ano_atual - ano_nasc
+    if idade > 18:
+        barbeiros = [barbeiro, idade, ano_nasc]
+        lista_principal.append(barbeiros)
+    else:
+        print("Barbeiro não pode ser menor de idade! ")
+        break
+
+    
+for barbeiro in lista_principal:
+    print(lista_principal)
+
+
+
+# 
