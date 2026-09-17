61a0a41b47d911a10caed679c338b25a34d65ba7
Álvaro Yuri <alvaro.yuri@outlook.com>
2026-09-17 17:05:52 -0300

    deleta arquivos que não foram revisados para o escopo atual

--- barber_grid/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Agendamento, Cliente, Servico
-# Register your models here.
-
-admin.site.register(Agendamento)
-admin.site.register(Cliente)
-admin.site.register(Servico)
--- barber_grid/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BarberGridConfig(AppConfig):
-    name = 'barber_grid'
